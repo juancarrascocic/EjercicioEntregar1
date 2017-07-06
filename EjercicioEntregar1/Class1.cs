e4ce81e22db09bf4ea39a9c12c3ec270359370dd
juancarrascocic <juancarrascocic@gmail.com>
2017-07-06 17:48:04 +0200

    Commit para añadir los ficheros al completo

---
+++ EjercicioEntregar1/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioEntregar1
+{
+    public class ClassDatos
+    {
+        public String texto
+        {
+            set;
+            get;
+        }
+        public int entero1
+        {
+            set;
+            get;
+        }
+        public int entero2
+        {
+            set;
+            get;
+        }
+    }
+    public class ClassMetodos
+    {
+        public String MetodoConcatenar(ClassDatos objeto)
+        {
+            return objeto.texto + objeto.entero1 + " y "+ objeto.entero2;
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EjercicioEntregar1;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ClassDatos sut = new ClassDatos();
+            sut.texto = "Los numeros son: ";
+            sut.entero1 = 7;
+            sut.entero2 = 16;
+
+            ClassMetodos sut2 = new ClassMetodos();
+
+            String resultado = sut2.MetodoConcatenar(sut);
+            Assert.AreEqual(resultado, "Los numeros son: 7 y 16");
+        }
+    }
+}
